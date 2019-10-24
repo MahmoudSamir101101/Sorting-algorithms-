156445ad6af3d24ca1b7125143fbbb3b39b43f58
Tarek <mt.passerby@gmail.com>
2019-10-24 21:35:33 +0200

    Setting the project's structure

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,42 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sorting.algorithms.git;
-import java.util.Scanner;
-/**
- *
- * @author Trixy
- */
 public class SortingAlgorithms {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        //Insertion Sort
-        // TODO code application logic here
-        Scanner Enter=new Scanner(System.in);
-        System.out.println(" Enter the Numbers to sorted");
-        int i,j,key;
-        int x[]=new int[10];
-        for(int z=0;z<x.length;z++)
-            x[z]=Enter.nextInt();
-        i=1;
-        while(i<x.length){
-            key=x[i];
-            j=i-1;
-            while(j>=0&&x[j]>key){
-                x[j+1]=x[j];
-                j=j-1;
-                x[j+1]=key;}
-            i++;
-            }
-        for(int z=0;z<x.length;z++)
-            System.out.println(x[z]);
     }
-                
-    }
-    
 
+    public void SelectionSort(int[] arr){
+    }
+
+    public void InsertioSort(int[] arr){
+      int i,j,key;
+      i=1;
+      while(i<arr.length){
+          key=arr[i];
+          j=i-1;
+          while(j>=0&&arr[j]>key){
+              arr[j+1]=arr[j];
+              j=j-1;
+              arr[j+1]=key;}
+          i++;
+          }
+    }
+    public void BubbleSort(int[] arr){
+    }
+    //Follow the same structure when adding extra method
+    }
